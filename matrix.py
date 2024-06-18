5d078d4bdfd4168a5c82d736024a22bba0c0b6c4
Naseera07 <anaseerabegum@gmail.com>
2024-06-18 20:38:12 +0530

    Add files via upload

---
+++ matrix.py
@@ -0,0 +1,11 @@
+a=[]
+for i in range(0,2):
+    sub=[]
+    print("enter values for row",i)
+    for j in range(0,2):
+        print("enter values for column",j)
+        ele=int(input())
+        sub.append(ele)
+        print(sub)
+    a.append(sub)
+    print(a)
